bb311dcb040c96cbe04aceb38ca06b77c89af31a
Alex Maldonado <alex.maldonado@outlook.com>
2021-03-02 22:33:49 -0300

    Add metadata and referral links to portfolio

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -16,17 +16,19 @@
           <div class="flex justify-end items-center relative">
 
             <div class="flex mr-4 items-center">
-              <a class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" href="#">
+              <a class="inline-block font-bold py-2 px-3 hover:bg-yellow-400 rounded-full hover:text-black" href="https://www.cisnedigital.cl/portafolio" target="_blank" title="Visitar Portafolio">
                 <div class="flex items-center relative cursor-pointer whitespace-nowrap">Portfolio</div>
               </a>
               <div class="block relative">
-                <button type="button" class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full relative ">
-                  <div class="flex items-center h-5">
-                    <div class="_xpkakx">
-                      <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 16px; width: 16px; fill: currentcolor;"><path d="m8.002.25a7.77 7.77 0 0 1 7.748 7.776 7.75 7.75 0 0 1 -7.521 7.72l-.246.004a7.75 7.75 0 0 1 -7.73-7.513l-.003-.245a7.75 7.75 0 0 1 7.752-7.742zm1.949 8.5h-3.903c.155 2.897 1.176 5.343 1.886 5.493l.068.007c.68-.002 1.72-2.365 1.932-5.23zm4.255 0h-2.752c-.091 1.96-.53 3.783-1.188 5.076a6.257 6.257 0 0 0 3.905-4.829zm-9.661 0h-2.75a6.257 6.257 0 0 0 3.934 5.075c-.615-1.208-1.036-2.875-1.162-4.686l-.022-.39zm1.188-6.576-.115.046a6.257 6.257 0 0 0 -3.823 5.03h2.75c.085-1.83.471-3.54 1.059-4.81zm2.262-.424c-.702.002-1.784 2.512-1.947 5.5h3.904c-.156-2.903-1.178-5.343-1.892-5.494l-.065-.007zm2.28.432.023.05c.643 1.288 1.069 3.084 1.157 5.018h2.748a6.275 6.275 0 0 0 -3.929-5.068z"></path></svg>
-                    </div>
-                  </div>
-                </button>
+                  <a href="https://github.com/alexmaldonado-cl/todo-list-livewire" title="Ver proyecto en GitHub" target="_blank">
+                    <button type="button" class="inline-block py-2 px-3 hover:bg-yellow-400 rounded-full relative">
+                        <div class="flex items-center h-5">
+                          <div class="_xpkakx">
+                              <svg width="24" height="24" fill="currentColor" class="text-white hover:text-black"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z"></path></svg>
+                          </div>
+                        </div>
+                      </button>
+                  </a>
               </div>
             </div>
 
